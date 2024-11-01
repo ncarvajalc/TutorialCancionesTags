ae7b1d3738d96fbff6dbd30a5c855c0331f6b08c
Nicolás Carvajal <n.carvajalc@uniandes.edu.co>
2024-11-01 14:42:27 -0500

    Added bug for sonar to detect

--- src/modelo/declarative_base.py
+++ src/modelo/declarative_base.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DB_PASSWORSD = "admin"
 engine = create_engine("sqlite:///aplicacion.sqlite")
+engine_copy = engine
+
+print(engine=engine_copy)
+
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
